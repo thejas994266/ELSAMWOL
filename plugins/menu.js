b504a9620a1e713eda03d39870002e34d9c5ebd8
SKDRAGON1 <93061627+SKDRAGON1@users.noreply.github.com>
2021-11-11 09:04:01 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -23,37 +23,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
 𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 *AJNAS SK* 𝐛𝐨𝐭.
             *ELSAMWOL*
 
- text  on 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -dict 
-🍙𝙐𝙨𝙚➜   Dictionary [-dict en;anime]
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -zodiac <leo> 
-🍙𝙐𝙨𝙚➜   Tells you about your horoscope.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -qr <text>
-🍙𝙐𝙨𝙚➜   Converts text into qr code.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -movie 
-🍙𝙐𝙨𝙚➜  Gives you info about movie.
-⚠️movie master
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -anime <text>
-🍙𝙐𝙨𝙚➜  Gives you info about anime.
-⚠️anime pikachu
-■□■□■□■□■Pikachu□■□■□■□■□■□
-♝ *𝙎𝙩𝙞𝙘𝙠𝙚𝙧 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♝
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -sticker 
-🍙𝙐𝙨𝙚➜ Converts img/gif into a sticker.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -photo 
-🍙𝙐𝙨𝙚➜ Converts sticker into image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -attp <text>
-🍙𝙐𝙨𝙚➜ Converts text into glowing sticker.
-⚠️ex  attp Elsa Mwol
-■□■□■□■□■□■□■□■□■□■□
-
+ 
 ▣▣▣▣▣▣▣▣▣✩ᴀᴊɴᴀꜱ ꜱᴋ✩▣▣▣▣▣▣▣▣▣▣▣
 
 `}) 
