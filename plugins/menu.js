b5634ff978e6081e0fe8d40fd3af4f827e78c086
SKDRAGON1 <93061627+SKDRAGON1@users.noreply.github.com>
2021-11-12 08:43:33 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -7,14 +7,7 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
-    var r_text = new Array ();
     
-    
-    r_text[0] = "https://i.imgur.com/28KsnoW.mp4";
-    
-    
-    var i = Math.floor(1*Math.random())
-
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️AJNAS SK✨️━━━━━⛦
